30388085c2d002b4ba6622ef39dfa544cdbf02c4
Augusto12 <felipe_hanonimous@hotmail.com>
2023-04-09 13:50:11 -0300

    Refactoring data access

---
+++ utils/datastore.py
@@ -0,0 +1,39 @@
+import requests
+import pandas as pd 
+from io import StringIO
+
+ENDPOINT = 'https://archive.ics.uci.edu/ml/machine-learning-databases/image/'
+TRAIN_FILE = 'segmentation.data'
+TEST_FILE = 'segmentation.test'
+
+class DataStore:
+
+    def __init__(self,endpoint=ENDPOINT,files=[TRAIN_FILE,TEST_FILE]):
+        self._load_data(endpoint,files)
+
+    def _etl(self,endpoint,file):
+        response = requests.get(endpoint + file)
+        if response.ok:
+            rows = str(response.content).split('\\n')
+            header = [rows[3]]
+            data = rows[5:-2]
+            return "\n".join(header + data)
+        else:
+            raise Exception("TODO")
+
+    def _load_data(self,endpoint,files):
+        dataframes = [pd.read_csv(StringIO(self._etl(endpoint,file))) for file in files]
+        self.data = pd.concat(dataframes)
+
+    def create_datasets(self):
+        '''
+        D1 = 
+        D2 = 
+        D3 = 
+        return [D1,D2,D3]
+        '''
+        pass
+        
+
+        
+    
