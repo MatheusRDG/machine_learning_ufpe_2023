3f2e2ebeeda57ba4222a09fe53f9093e6d6f5870
Augusto12 <felipe_hanonimous@hotmail.com>
2023-04-10 23:10:52 -0300

    Add local storage and data and conf to DS

--- utils/datastore.py
+++ utils/datastore.py
@@ -1,15 +1,21 @@
 import requests
 import pandas as pd 
+import os
 from io import StringIO
 
-ENDPOINT = 'https://archive.ics.uci.edu/ml/machine-learning-databases/image/'
-TRAIN_FILE = 'segmentation.data'
-TEST_FILE = 'segmentation.test'
+conf = {
+    'endpoint': 'https://archive.ics.uci.edu/ml/machine-learning-databases/image/',
+    'files': ['segmentation.data','segmentation.test'],
+    'local_datapath': '../local/',
+    'datafile': 'data.csv',
+    'seed': 11
+}
 
 class DataStore:
 
-    def __init__(self,endpoint=ENDPOINT,files=[TRAIN_FILE,TEST_FILE]):
-        self._load_data(endpoint,files)
+    def __init__(self,conf=conf):
+        self._load_data(conf['endpoint'],conf['files'],conf['local_datapath'],conf['datafile'])
+        self.seed = conf['seed']
 
     def _etl(self,endpoint,file):
         response = requests.get(endpoint + file)
@@ -21,9 +27,16 @@ class DataStore:
         else:
             raise Exception("TODO")
 
-    def _load_data(self,endpoint,files):
-        dataframes = [pd.read_csv(StringIO(self._etl(endpoint,file))) for file in files]
-        self.data = pd.concat(dataframes)
+    def _load_data(self,endpoint,files,datapath,datafile):
+        filepath = datapath + datafile
+        if not os.path.exists(filepath):
+            if not os.path.exists(datapath):
+                os.makedirs(datapath)
+            dataframes = [pd.read_csv(StringIO(self._etl(endpoint,file))) for file in files]
+            self.data = pd.concat(dataframes)
+            self.data.to_csv(filepath)
+        else:
+            self.data = pd.read_csv(filepath)
 
     def build_datasets(self):
         D1 = self.data.iloc[:,4:9]
